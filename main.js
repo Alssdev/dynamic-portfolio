7ce8f568c593e52272aae82f8b16f132a9509f25
alssdev <alssdev3@gmail.com>
2020-08-19 20:28:19 -0600

    implementación del overlay

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const links = document.querySelectorAll('#categories a');
+
 const searchBar = document.getElementById('search-bar');
+
+const items = document.querySelectorAll('.grid .item img');
+const overlay = document.getElementById('overlay');
 
 const grid = new Muuri('.grid', {
 	layout: {
@@ -39,4 +43,24 @@ searchBar.addEventListener('input', (e) => {
 	const label = e.target.value.toLowerCase();
 
 	grid.filter((item) => item.getElement().dataset.labels.includes(label));
+});
+
+items.forEach((item) => {
+	const url = item.getAttribute('src');
+	const desc = item.parentElement.parentElement.getAttribute('data-desc');
+
+	item.addEventListener('click', () => {
+		overlay.classList.add('active');
+
+		document.querySelector('#overlay img').src = url;
+		document.querySelector('#overlay p').innerHTML = desc;
+	});
+});
+
+document.querySelector('#close-popup').addEventListener('click', () => {
+	overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', (e) => {
+	if (e.target.id === 'overlay') overlay.classList.remove('active');
 });
