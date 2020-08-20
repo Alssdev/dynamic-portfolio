6cd3f18b988e94b45566d07b79c186a91255b87a
alssdev <alssdev3@gmail.com>
2020-08-19 19:49:36 -0600

    filter images

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const links = document.querySelectorAll('#categories a');
+const searchBar = document.getElementById('search-bar');
+
 const grid = new Muuri('.grid', {
 	layout: {
 		rounding: false,
@@ -7,4 +10,33 @@ const grid = new Muuri('.grid', {
 window.addEventListener('load', () => {
 	grid.refreshItems().layout();
 	document.getElementById('grid').classList.add('uploaded-images');
+});
+
+links.forEach((link) => {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		// remove active class for others links
+		links.forEach((l) => {
+			l.classList.remove('active');
+		});
+
+		// set class active to clicked link
+		e.target.classList.add('active');
+
+		const category = e.target.innerHTML.toLowerCase();
+
+		// filter by category
+		if (category !== 'all') {
+			grid.filter(`[data-category="${category}"]`);
+		} else {
+			grid.filter(`[data-category]`);
+		}
+	});
+});
+
+searchBar.addEventListener('input', (e) => {
+	const label = e.target.value.toLowerCase();
+
+	grid.filter((item) => item.getElement().dataset.labels.includes(label));
 });
